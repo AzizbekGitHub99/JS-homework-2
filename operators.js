b5f72096416b1fc9402ddc89c62c0f7a8a87627c
AzizbekGitHub99 <108000317+AzizbekGitHub99@users.noreply.github.com>
2023-12-09 20:48:37 +0500

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,85 @@
+// №1
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+
+// Y = -(B / 2) * (Math.pow(A,2) + Math.pow(B,2));
+
+// №2
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+// let C = +prompt("C son kiriting!")
+// let D = +prompt("D son kiriting!")
+
+// Y = Math.pow(((A + B) / (C + D)),2) + Math.pow(B,2);
+
+// №3
+
+// let A = +prompt("A son kiriting!")
+// let C = +prompt("C son kiriting!")
+// let D = +prompt("D son kiriting!")
+
+// Y = 2 * (Math.pow(A,D) + (4 * Math.pow(C,2)) / 3);
+
+// №4
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+// let C = +prompt("C son kiriting!")
+// let D = +prompt("D son kiriting!")
+
+// Y = Math.pow(A,-2) * (Math.pow(B / 10,3)) * Math.pow(C + D,2);
+
+// №5
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+// let C = +prompt("C son kiriting!")
+// let D = +prompt("D son kiriting!")
+
+// Y = Math.pow(((A / B) - 1) / (C / (D - 1)),2);
+
+// №6
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+// let C = +prompt("C son kiriting!")
+// let D = +prompt("D son kiriting!")
+
+// Y = (A + B) / (C + (D / (A + C)));
+
+// №7
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+// let C = +prompt("C son kiriting!")
+
+// Y = Math.sqrt(Math.pow((A + B + C),2) - Math.pow((A - B - C),2));
+
+// №8
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+// let C = +prompt("C son kiriting!")
+
+// Y = (Math.pow(A,2) + Math.pow(B,2) + Math.pow(C,2)) / (A * B * C);
+
+// №9
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+// let C = +prompt("C son kiriting!")
+// let D = +prompt("D son kiriting!")
+
+// Y = A * (Math.pow(D,2) / 3) + B * (Math.pow(C,2) / 2);
+
+// №10
+
+// let A = +prompt("A son kiriting!")
+// let B = +prompt("B son kiriting!")
+// let C = +prompt("C son kiriting!")
+
+// Y = Math.abs((A / B) - (B / C)) + Math.pow((A + B),1 / 4);
+
+console.log("Funksiyaning javobi: " + Y);
